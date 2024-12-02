60ad04acd1988d2e03e9ff95325084e3c77081bf
sowgand <ramezanzadesogand@gmail.com>
2024-12-02 17:35:57 +0330

    Update 2sum.js

--- 2sum.js
+++ 2sum.js
@@ -23,6 +23,9 @@ function twosum (numbers , target) {
     for (let i  = 0 ; i  < array.length; i ++) {
       for (let j = i+1 ; j  < array.length; j ++) {
         
+        if ( numbers[i] + numbers[j] === target) {
+            
+        }
         
       }
         
