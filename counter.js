0d8f0b1575c777c5321eb5db8e08fc6794e4679e
sowgand <ramezanzadesogand@gmail.com>
2024-12-03 14:36:13 +0330

    Create counter.js

---
+++ counter.js
@@ -0,0 +1,12 @@
+//Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+//Example 1:
+
+//Input: 
+//n = 10 
+//["call","call","call"]
+//Output: [10,11,12]
+
+function counter (n) {
+    
+}
